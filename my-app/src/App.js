d70e5def85fc724ba3a2f571e6070e4b687be21f
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-10-16 07:13:31 +0200

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Contacts from './components/Contacts';
+import ContactForm from './components/ContactForm';
 import data from './data/data'
 
 class App extends Component {
 	render() {		
 		return (			
 			<div>
+				<div><ContactForm /></div>
 				<div><Contacts data={data}/></div>
 			</div>		
 		)
